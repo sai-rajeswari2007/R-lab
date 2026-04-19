af8219dc68f1438f0c3b500d21a712c3d4055973
sai-rajeswari2007 <velagapudisairajeswari@gmail.com>
2026-04-19 19:28:20 +0530

    Create EX_6.R

---
+++ EX_6.R
@@ -0,0 +1,121 @@
+\\
+
+\\1.
+Vector is suitable because all values are of the same data type.
+marks <- c(78, 85, 90, 88, 76)
+total <- sum(marks)
+average <- mean(marks)
+highest <- max(marks)
+total
+average
+higher 
+
+\\2.
+temperature <- c(34, 35, 36, 38, 39, 40, 73)
+min_temp <- min(temperature)
+max_temp <- max(temperature)
+min_temp
+max_temp
+
+\\3.
+List is suitable because it supports different data types.
+employee <- list(
+ID = 101,
+Name = "srinidhi",
+Salary = 5000,
+Department = "cse"
+)
+employee
+employee$Name
+employee$salary
+
+\\4.
+patient <- list(
+PatientID = 106,
+Name = "rahul",
+Age = 15,
+TestResults = c(10, 14, 30)
+)
+patient
+patient$TestResults
+
+\\5.
+Data frame is best for structured tabular data.
+students <- data.frame(
+  RollNo = c(1, 2, 3),
+Name = c("Ajay", "Krithika", "raja"),
+Dept = c("CSE", "ECE", "EEE"),
+Marks = c(56, 57, 72)
+)
+students
+
+\\6.
+sales <- data.frame(
+Month = c("mar", "june", "feb"),
+Sales = c(5000, 6000, 8000)
+)
+total_sales <- sum(sales$Sales)
+total_sales
+
+\\7.
+prices <- c(1200, 850, 560)
+customer <- list(
+CustomerID = 301,
+Name = "Anju",
+City = "andhra"
+)
+orders <- data.frame(
+Item = c("Phone", "Headset", "Charger"),
+Price = prices
+)
+prices
+customer
+orders
+
+\\8.
+subject_marks <- c(78, 85, 88)
+student_profile <- list(
+Name = "Alleka",
+RollNo = 209,
+Marks = subject_marks
+)
+class_results <- data.frame(
+RollNo = c(101, 102, 103),
+Total = c(240, 255, 270)
+)
+student_profile
+class_results
+
+
+\\9.
+balances <- c(5000, 5200, 5100)
+customer <- list(
+AccountNo = 12345,
+Name = "Suresh"
+)
+transactions <- data.frame(
+Date = c("01-01", "02-01", "03-01"),
+Amount = c(-500, 1000, -200)
+)
+balances
+customer
+transactions
+
+\\10.
+Vector stores homogeneous data (marks, prices)
+List stores heterogeneous data (profiles)
+Data Frame stores structured tabular data (records)
+scores <- c(85, 90, 88)
+profile <- list(
+Name = "Gayathri",
+Age = 21,
+Scores = scores
+ )
+records<data.frames
+sub=(tel,hin,math,sci(
+marks=scores
+)
+
+  
+
+  
