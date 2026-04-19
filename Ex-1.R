1901d8eeffc30265368560dede1fea284af363dc
sai-rajeswari2007 <velagapudisairajeswari@gmail.com>
2026-04-19 18:28:18 +0530

    Create Ex-1.R with various data structures
    Added multiple data entries including student, employee, product, city, and account information.

---
+++ Ex-1.R
@@ -0,0 +1,80 @@
+\\1:
+roll_no <- 101
+name <- "Anitha"
+cgpa <- 8.45 
+pass <- TRUE
+roll_no
+name
+cgpa
+pass
+
+\\2:
+emp_id <- 5001
+salary <- 45000.75
+department <- "IT"
+is_permanent <- TRUE
+emp_id
+salary
+department
+is_permanent
+
+\\3:
+product_name <- "Laptop"
+price <- 55000
+stock_available <- 25
+discount_eligible <- FALSE
+product_name
+price
+stock_available
+discount_eligible
+
+\\4:
+city <- "Chennai"
+temperature <- 36.5
+rain <- FALSE
+wind_speed <- 12.8
+city
+temperature
+rain
+wind_speed
+
+\\5:
+total_marks <- 100
+marks_obtained <- 78
+result <- marks_obtained >= 40
+subject
+total_marks
+marks_obtained
+result
+
+\\6:
+Answer:
+account_no <- 123456789
+holder_name <- "Ravi Kumar"
+balance <- 25000.50
+account_active <- TRUE
+account_no
+holder_name
+balance
+account_active
+
+\\7:
+Answer:
+movie_title <- "Inception"
+rating <- 8.8
+reviews <- 1200
+available <- TRUE
+movie_title
+rating
+reviews
+available
+
+\\8:
+vehicle_count <- 350
+location <- "Highway Road"
+avg_speed <- 45.6
+congested <- TRUE
+vehicle_count
+location
+avg_speed
+congested
